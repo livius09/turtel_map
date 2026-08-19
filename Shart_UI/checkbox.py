fb69e60ade6575cb19cb037f29f850d0c5b74a08
livius09 <levi.wimroither@hotmail.com>
2026-08-19 14:50:26 +0200

    implemented checkbox

---
+++ Shart_UI/checkbox.py
@@ -0,0 +1,45 @@
+import turtle as t
+from Node import Node
+import math
+
+class checkbox(Node):
+    is_ticked:bool=False
+    
+
+    def __init__(self):
+        super().__init__()
+        self.outline=True
+        self.width=10
+        self.height=10
+
+    def onclick(self):
+        super().onclick()
+        self.is_ticked= not self.is_ticked
+        self.mdirty()
+
+    def set_scale(self,new_scale:float):
+        num = round(10*new_scale)
+
+        self.width=num
+        self.height=num
+        self.mdirty()
+
+    def render_self(self):
+        super().render_self()
+
+        if self.is_ticked:
+            dia = math.sqrt(self.width**2 + self.height**2)
+            t.teleport(self.x,self.y)
+            t.seth(45)
+            t.pendown()
+            t.forward(dia)
+            t.teleport(self.x, self.y+self.height)
+            t.seth(315)
+            t.forward(dia)
+
+            t.seth(0)
+            t.penup()
+
+
+
+
--- Shart_UI/users_code.py
+++ Shart_UI/users_code.py
@@ -2,6 +2,7 @@ from main import app
 from scene import scene
 from lable import lable
 from Button import Button
+from checkbox import checkbox
 
 myapp = app()
 
@@ -17,7 +18,7 @@ frontscene.add_node(mylb)
 
 
 seclb = lable("move")
-seclb.set_xy(40,40)
+seclb.set_xy(20,40)
 seclb.set_outline(True)
 
 seclb.onClick_func = lambda : seclb.set_x(seclb.x+5)
@@ -29,6 +30,15 @@ mybtn.set_xy(20,60)
 mybtn.onClick_func = lambda : print("Dong")
 
 frontscene.add_node(mybtn)
+
+mycheck = checkbox()
+mycheck.set_xy(20,80)
+
+frontscene.add_node(mycheck)
+
+
+
+
 
 myapp.set_scene(frontscene)
 
